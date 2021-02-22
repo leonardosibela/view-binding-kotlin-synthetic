b8170efbfc2ebc4b3841409a77cbfa974baacb8c
leonardosibela <leonardo_sibela@hotmail.com>
2021-02-22 11:42:04 -0300

    Adding bindings with Kotlin Synthetic

--- app/src/main/java/com/example/viewbindingkotlinsynthetic/MainActivity.kt
+++ app/src/main/java/com/example/viewbindingkotlinsynthetic/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.viewbindingkotlinsynthetic
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        logoImage.setBackgroundResource(R.drawable.android)
+
+        nameText.text = "Felix Arvid Ulf Kjellberg"
+        nicknameText.text = "PewDiePie"
+        countryText.text = "Sweden"
+
+        styleOneText.text = "Games"
+        styleTwoText.text = "Comedy"
+        styleThreeText.text = "React"
+        styleFourText.text = "Terror"
+
+        clickmeButton.setOnClickListener {
+            Log.e("TAG", "Hello!")
+        }
     }
 }
